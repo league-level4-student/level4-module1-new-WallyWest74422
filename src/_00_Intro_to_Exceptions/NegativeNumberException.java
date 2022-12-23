b103d1d35a8f84908a018ae2d2a1951a786f781e
WallyWest74422 <53949174+WallyWest74422@users.noreply.github.com>
2022-12-22 18:33:51 -0800

    Startedw/Exceptions

---
+++ src/_00_Intro_to_Exceptions/NegativeNumberException.java
@@ -0,0 +1,25 @@
+package _00_Intro_to_Exceptions;
+
+import javax.swing.JOptionPane;
+
+public class NegativeNumberException extends Exception {
+
+void scaryPopUp () {
+	JOptionPane.showMessageDialog(null, "You have triggered a critical error in your computer :0!");
+}
+
+static void testPositive (int i) throws NegativeNumberException {
+	if(i<0) {
+		throw new NegativeNumberException();
+	}
+}
+public static void main(String[] args) {
+	try {
+		testPositive(-2);
+	} catch (NegativeNumberException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+
+	}
+}
+}
